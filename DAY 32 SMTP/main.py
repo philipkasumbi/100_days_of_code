d5b35abb92a311ce2d7035a5094c7d0edd42ea9e
Philip Kasumbi <philipkasumbi8@gmail.com>
2026-06-07 09:41:33 +0300

    refactor: send emails using EmailMessage

--- DAY 32 SMTP/main.py
+++ DAY 32 SMTP/main.py
@@ -1,13 +1,38 @@
+import datetime as dt
+import random
 import smtplib
+from email.message import EmailMessage
+
+from multiprocessing import connection
 
 my_email = "pythont695@gmail.com"
 password = "juid frmf tund wnwy"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email,password=password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="kasumbipeter5@gmail.com",
-    msg="Subject:Hello\n\nThis is a test email from Python.")
-connection.close()
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="kasumbipeter5@gmail.com",
+#         msg="Subject:Hello\n\nThis is a test email from Python.")
+
+now= dt.datetime.now()
+day_of_week = now.weekday()
+
+with open("quotes.txt","r",encoding="utf-8") as quotes:
+    content = quotes.readlines()
+    random_quote = random.choice(content)
+
+message = EmailMessage()
+message["Subject"] ="Sunday Motivation"
+message["From"] = my_email
+message["To"] = "kasumbipeter5@gmail.com"
+message.set_content(random_quote)
+
+if day_of_week == 6:
+    with smtplib.SMTP("smtp.gmail.com",587) as connection:
+        connection.starttls()
+        connection.login(user=my_email,password=password)
+        connection.send_message(message)
+
+
