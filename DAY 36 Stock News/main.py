c1a59b727e87380113698c40e77a6ad0b681c425
Philip Kasumbi <philipkasumbi8@gmail.com>
2026-06-15 18:30:55 +0300

    feat: integrate Tesla stock movement alerts with news aggregation and SMS delivery

--- DAY 36 Stock News/main.py
+++ DAY 36 Stock News/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+from twilio.rest import Client
 
 """
 One Stock
@@ -10,14 +12,14 @@ Get News
 Send Notification
 """
 
-API_KEY = "4VG9N5R4AZIRBBSY"
-parameters = {
+stock_key = os.environ["STOCK_KEY"]
+stock_parameters = {
     "function":"TIME_SERIES_DAILY",
     "symbol":"TSLA",
-    "apikey":API_KEY
+    "apikey":stock_key
 }
 url = 'https://www.alphavantage.co/query'
-response = requests.get(url,params=parameters)
+response = requests.get(url,params=stock_parameters)
 daily_data = response.json()["Time Series (Daily)"]
 
 dates = list(daily_data.keys())
@@ -28,11 +30,56 @@ latest = sorted_dates[0]
 previous = sorted_dates[1]
 
 # opening and closing values
-today_closing = daily_data[latest]["4. close"]
-yesterday_closing = daily_data[previous]["4. close"]
+today_closing = float(daily_data[latest]["4. close"])
+yesterday_closing = float(daily_data[previous]["4. close"])
 
 # percentage change
 value_change = today_closing-yesterday_closing
 percentage_change = (value_change/yesterday_closing)*100
+
+# news
+news_key = os.environ["NEWS_KEY"]
+
+news_parameters = {
+    "q":"tesla",
+    "from":previous,
+    "sortBy":"publishedAt",
+    "apiKey":news_key
+}
+url = "https://newsapi.org/v2/everything"
+
+if abs(percentage_change) > 5:
+
+    news = requests.get(url, params=news_parameters)
+    news_data = news.json()["articles"]
+
+    message = ""
+
+    for news in news_data[:3]:
+        title = news["title"]
+        description = news["description"]
+        url = news["url"]
+        news_message =(f"\n\ntitle:{title},\ndescription:{description},\nread more:{url}")
+        message += news_message
+
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body="message",
+        from_="+19086417409",
+        to="+254790553620",
+    )
+
+
+
+
+
+
+
+
+
+
 
 
